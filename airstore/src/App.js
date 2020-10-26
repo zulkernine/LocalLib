09e7d536dfd51ab5836d1d25219f30e965a26c8e
zulkernine <mohiuddinmondal107@gmail.com>
2020-10-27 00:21:52 +0530

    airstore

---
+++ airstore/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Album from './album';
+
+function App() {
+  return (
+    <div className="App">
+      <Album/>
+    </div>
+  );
+}
+
+export default App;
